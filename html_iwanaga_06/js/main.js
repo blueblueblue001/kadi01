0870b63d42b0c99c4a1fe2bc514d5ea0a80666d9
blueblueblue001 <131594189+blueblueblue001@users.noreply.github.com>
2023-05-03 18:26:00 +0900

    Cheese Academy HW submission

---
+++ html_iwanaga_06/js/main.js
@@ -0,0 +1,7 @@
+// jsを記述する際はここに記載していく
+const ham = $('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
+const nav = $('#js-nav'); //js-navの要素を取得し、変数navに格納
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+  console.log("ok!"); //コンソール画面でokというメッセージが出る
+ 
+});
